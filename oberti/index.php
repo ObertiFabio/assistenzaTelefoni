7b378d6f0e8d29cf71e0d5562ecb797adc691b8f
ObertiFabio <101709153+ObertiFabio@users.noreply.github.com>
2023-12-18 13:22:17 +0100

    aggiunta funzionalità visualizza profilo

---
+++ oberti/index.php
@@ -0,0 +1,16 @@
+<?php
+    session_start();
+    require 'connessione.php';
+
+    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+        header("location: login.php");
+        exit;
+    }
+?>
+<html>
+    <body>
+        <h1>Benvenuto!!</h1>
+        <a href="logout.php">Logout</a>
+        <a href="profilo.php">Visualizza il tuo profilo</a>
+    </body>
+</html>
