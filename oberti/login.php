e2481fcbdc89d5abc29527fd8fe7574bc4ad96e5
ObertiFabio <101709153+ObertiFabio@users.noreply.github.com>
2024-01-18 11:54:40 +0100

    presa  a carico di una richiesta da parte di un manutentore

---
+++ oberti/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form method="POST" action="loginController.php">
+        <label for="matricola">Matricola:</label>
+        <input type="text" id="matricola" name="matricola" required><br><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br><br>
+        <input type="submit" value="Login">
+    </form>
+    <br>
+    <a href="registra.php"><button>Registrati</button></a>
+    <?php
+        if (isset($_GET['error']) && $_GET['error'] == 1) {
+            echo $_GET['error'];
+            echo "<br><br><p style='color: red;'>Errore: Matricola o password non validi. Perfavore riprovare!</p>";
+        }
+        if (isset($_GET['error']) && $_GET['error'] == 101) {
+            echo $_GET['error'];
+            echo "<br><br><p style='color: red;'>Errore: Matricola o password non validi. Perfavore riprovare!</p>";
+        }
+    ?>
+</body>
+</html>
---
+++ oberti/update_manutentore.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    require 'connessione.php';
+    echo "entrato";
+
+    if(!isset($_POST["id_richiesta"])){
+       $id=$_POST["id"];
+       $manutentore_id= $_SESSION["matricola"];
+
+       $sql= "UPDATE richiesta SET manutentore_id = ?, stato='presa in carico' WHERE id_richiesta = ?";
+       $stmt = $connessione->prepare($sql);
+       $stmt->bind_param("ss", $manutentore_id, $id);
+
+        if($stmt->execute()){
+            echo "Richiesta presa in carico";
+        } else{
+            echo "Errore: " . $sql . "<br>" . $connessione->error;
+        }
+        exit;
+    }
+?>
